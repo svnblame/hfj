47cb62c31ccd08a842f4ad2d3b68180d3146c7b9
Gene <gene@phpcodehead.com>
2014-07-20 12:30:43 -0500

    Chapter 10: Numbers and Statics: Numbers Matters - Exercise code 02 (Finished chapter 10)

---
+++ HeadFirstJava/src/Excersises/Chapter10/StaticTests.java
@@ -0,0 +1,22 @@
+package Excersises.Chapter10;
+
+/**
+ * Created by Gene on 7/20/2014.
+ */
+public class StaticTests {
+    static int rand;
+
+    static {
+        rand = (int) (Math.random() * 6);
+        System.out.println("static block " + rand);
+    }
+
+    StaticTests() {
+        System.out.println("constructor");
+    }
+
+    public static void main(String [] args) {
+        System.out.println("in main");
+        StaticTests st = new StaticTests();
+    }
+}
