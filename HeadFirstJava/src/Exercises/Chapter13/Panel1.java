7a20897ac105a2f2928115307a652426890195f1
Gene <gene@phpcodehead.com>
2014-10-10 16:12:22 -0500

    Added Chapter 13, Exercises and "Cyber BeatBox" app.

---
+++ HeadFirstJava/src/Exercises/Chapter13/Panel1.java
@@ -0,0 +1,42 @@
+package Exercises.Chapter13;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel1 {
+
+    public static void main (String[] args) {
+        Panel1 gui = new Panel1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+
+        /**
+         * Set the layout manager to be a new
+         * instance of BoxLayout
+         */
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        /**
+         * Add buttons to the panel and add the
+         * panel to the frame. The panel's layout manager
+         * (flow) controls the button, and the frame's
+         * layout manager (border) controls the panel.
+         */
+        JButton button = new JButton("shock me");
+        JButton buttonTwo = new JButton("bliss");
+        JButton buttonThree = new JButton("huh?");
+        panel.add(button);
+        panel.add(buttonTwo);
+        panel.add(buttonThree);
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+
+        frame.setSize(250,200);
+        frame.setVisible(true);
+    }
+
+}
